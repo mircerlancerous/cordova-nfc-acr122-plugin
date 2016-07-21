2a55e6aa70615511ddd64949fdf496e4e419331b
OffTheBricks <mircerlancerous@users.noreply.github.com>
2016-07-21 16:00:07 -0700

    Update plugin.js

--- www/plugin.js
+++ www/plugin.js
@@ -1,9 +1,9 @@
 var nfcPlugin = {
-  stopListen = function(onSuccess, onFail){
+  stopListen: function(onSuccess, onFail){
     cordova.exec(onSuccess, onFail, 'NfcAcr122Plugin', 'stopListen', []);
   },
   
-  startListen = function(callback, onSuccess, onFail){
+  startListen: function(callback, onSuccess, onFail){
     document.addEventListener("tag", callback, false);
     cordova.exec(onSuccess, onFail, 'NfcAcr122Plugin', 'startListen', []);
   }
