f3fdf92d3164f040e3b6dcebf2b424348750890e
OffTheBricks <dave@offthebricks.com>
2017-07-12 14:08:13 -0700

    need this for usb permission

--- src/android/NfcAcr122Plugin.java
+++ src/android/NfcAcr122Plugin.java
@@ -2,7 +2,7 @@ package com.otb.cordova.nfc;
 
 import android.app.Activity;
 import android.app.PendingIntent;
-//import android.content.BroadcastReceiver;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
